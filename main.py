e7206a02a87081bff19d96e309d0286e1e9c4185
Dusadee-30 <63010356@kmitl.ac.th>
2021-11-10 03:29:24 +0700

    .

--- main.py
+++ main.py
@@ -48,9 +48,9 @@ count = 0
 for item in r["items"]:
     print(item['item_basic']['name'])
     if (int(item['item_basic']['price_min']) != int(item['item_basic']['price_max'])):
-        print("price min : ",int(item['item_basic']['price_min'])/100000," |  price max : ",int(item['item_basic']['price_max'])/100000)
+        print("price min : ",int(item['item_basic']['price_min'])/100000,"บาท |  price max : ",int(item['item_basic']['price_max'])/100000 ,"บาท")
     else:
-        print("price : ",int(item['item_basic']['price'])/100000)
+        print("price : ",int(item['item_basic']['price'])/100000,"บาท")
     lst.append(item['item_basic']['name'])
     #df = pd.DataFrame(get_ratings(item["shopid"], item["itemid"]))
     #print(df.head()) # print only the head for brevity
