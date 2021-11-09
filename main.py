ed7214703fc1c97b815a68c91b5a11497d07c08d
Dusadee-30 <63010356@kmitl.ac.th>
2021-11-10 03:25:55 +0700

    .

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import requests
 import pandas as pd
 
-
+#test
 def get_ratings(shop_id, item_id):
     ratings_url = "https://shopee.sg/api/v2/item/get_ratings?filter=0&flag=1&itemid={item_id}&limit=20&offset={offset}&shopid={shop_id}&type=0"
 
@@ -13,9 +13,6 @@ def get_ratings(shop_id, item_id):
         data = requests.get(
             ratings_url.format(shop_id=shop_id, item_id=item_id, offset=offset)
         ).json()
-
-        # uncomment this to print all data:
-        # print(json.dumps(data, indent=4))
 
         i = 1
         for i, rating in enumerate(data["data"]["ratings"], 1):
@@ -30,27 +27,33 @@ def get_ratings(shop_id, item_id):
 
     return d
 
+keyword_search = input("Search : ")
+print("-"*100)
 
 Shopee_url = "https://shopee.th"
-keyword_search = "headphone"
 headers = {
     "User-Agent": "Chrome",
     "Referer": "{}search?keyword={}".format(Shopee_url, keyword_search),
 }
 
-url = "https://shopee.co.th/api/v4/search/search_items?by=relevancy&keyword={}&limit=60&newest=0&order=desc&page_type=search&scenario=PAGE_GLOBAL_SEARCH&version=2".format(
+url = "https://shopee.co.th/api/v4/search/search_items?by=relevancy&keyword={}&limit=100&newest=0&order=desc&page_type=search&scenario=PAGE_GLOBAL_SEARCH&version=2".format(
     keyword_search
 )
-# can change "relevancy" to "latest": to sort by latest products instead
+
 
 # Shopee API request
 r = requests.get(url, headers=headers).json()
-
+lst = []
 count = 0
 for item in r["items"]:
     print(item['item_basic']['name'])
+    if (int(item['item_basic']['price_min']) != int(item['item_basic']['price_max'])):
+        print("price min : ",int(item['item_basic']['price_min'])/100000," |  price max : ",int(item['item_basic']['price_max'])/100000)
+    else:
+        print("price : ",int(item['item_basic']['price'])/100000)
+    lst.append(item['item_basic']['name'])
     #df = pd.DataFrame(get_ratings(item["shopid"], item["itemid"]))
     #print(df.head()) # print only the head for brevity
-    print("-" * 80)
+    print("-" * 100)
     count+=1
 print(count)
