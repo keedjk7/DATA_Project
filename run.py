4eeff803652797a0b576e4b98b08f90f5e08f712
keedjk7 <jirapat_kaewsongsang@hotmail.com>
2021-12-04 01:04:10 +0700

    pass

--- run.py
+++ run.py
@@ -4,67 +4,91 @@ import html
 import urllib
 import pandas as pd
 
-
-
 class node():
-    def __init__(self,data,next=None):
-        self.data=data
-        self.next=next
-class linklist():
+    def __init__(self,data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class link_list():
     def __init__(self):
-        self.head=None
-        self.tail=None
+        self.head = None
+
     def append(self,data):
-        newnode=node(data)
+        newNode = node(data)
         if self.head is None:
-            self.head=newnode
+            self.head = newNode
         else:
-            self.tail.next=newnode
-        self.tail=newnode
-    def __str__(self):
+            curr_node = self.head
+            while curr_node.next is not None:
+                curr_node = curr_node.next
+            curr_node.next = newNode
+
+    def __str__(self) :
         if self.head is None:
-            return ""
-        temp=str(self.head.data)
-        
-        last=self.head
-        while (last.next is not None):
-            temp=temp+" "+str(last.next.data)
-            
-            last=last.next
-            
+            return ''
+        temp = str(self.head.data)
+
+        last = self.head
+        while last.next is not None:
+            temp = temp + ' ' + str(last.next.data)
+            last = last.next
+
         return temp
-    def output(self):
-        if self.head is None:
-            return ""
-        item_dict=self.head.data
-        for i in item_dict:
-            print(i,':',item_dict[i])
-        print('-'*100)
-        last=self.head
-        while (last.next is not None):
-            item_dict=last.data
-            for i in item_dict:
-                print(i,':',item_dict[i])
-            print('-'*100)            
-            last=last.next
-            
+
     def isEmpty(self):
-        if self.head is None:
-            return True
-        else:
-            return False
-    def dequeue(self):
-        temp=self.head.data
-        self.head=self.head.next
-        return temp
+        return self.head == None
+
     def size(self):
-        size=0
-        last=self.head
-        while last is not None:
-            last=last.next
-            size=size+1
-        return  size
-    def sortMax(self):
+        count_size = 0
+        count_node = self.head
+        while count_node is not None:
+            count_size += 1
+            count_node = count_node.next
+
+        return count_size
+    
+    def sortPrice_min_to_max(self):
+        #bubble sort
+        count_node_for_check = self.head
+        while count_node_for_check is not None:
+            count_node = count_node_for_check.next
+            while count_node is not None:
+                if count_node_for_check.data['originPrice '] > count_node.data['originPrice ']:
+                    temp = count_node_for_check.data
+                    count_node_for_check.data = count_node.data
+                    count_node.data = temp
+
+                count_node = count_node.next
+                
+            count_node_for_check = count_node_for_check.next
+
+    def sortPrice_max_to_min(self):
+        #bubble sort
+        count_node_for_check = self.head
+        while count_node_for_check is not None:
+            count_node = count_node_for_check.next
+            while count_node is not None:
+                if count_node_for_check.data['originPrice '] < count_node.data['originPrice ']:
+                    temp = count_node_for_check.data
+                    count_node_for_check.data = count_node.data
+                    count_node.data = temp
+
+                count_node = count_node.next
+                
+            count_node_for_check = count_node_for_check.next
+
+    def sortRating_min_to_max(self):
+        for i in range(self.size()-1):
+            last=self.head
+            while last.next is not None:
+                if last.data["Rating      "] > last.next.data["Rating      "]:
+                    temp=last.data
+                    last.data=last.next.data
+                    last.next.data=temp
+                last=last.next 
+
+    def sortRating_max_to_min(self):
         for i in range(self.size()-1):
             last=self.head
             while last.next is not None:
@@ -73,23 +97,109 @@ class linklist():
                     last.data=last.next.data
                     last.next.data=temp
                 last=last.next
-    def sortMin(self):
-        for i in range(self.size()-1):
-            last=self.head
-            while last.next is not None:
-                if last.data["Rating      "] > last.next.data["Rating      "]:
-                    temp=last.data
-                    last.data=last.next.data
-                    last.next.data=temp
-                last=last.next   
 
+    def output(self):
+        if self.head is None:
+            return ''
+        item_dict = self.head.data
+
+        for i in item_dict:
+            print(i,':',item_dict[i])
+        print('-'*100)
+        count_node = self.head
+        while count_node is not None:
+            item_dict = count_node.data
+            for i in item_dict:
+                print(i,':',item_dict[i])
+            print('-'*100)
+            count_node = count_node.next
+
+
+def shopee(res_shopee,keyword_sort):
+    Shopee_ll = link_list()
+
+    print('\n*****SHOPEE*****\n')
+    for item in res_shopee["items"]:
+        dict_shopee_product = {}
+        image_link = (URL_IMAGE_SHOPEE+str(item['item_basic']['image']))
+        dict_shopee_product["Image_link  "] = image_link
+        dict_shopee_product["name        "] = item['item_basic']['name']
+        if (int(item['item_basic']['price_min']) != int(item['item_basic']['price_max'])):
+            dict_shopee_product['price       '] = "price min : "+str(int(item['item_basic']['price_min'])/100000)+"บาท |  price max : "+str(int(item['item_basic']['price_max'])/100000) +"บาท"
+            dict_shopee_product['originPrice '] = int(item['item_basic']['price_min'])/100000 #use price_min
+        else:
+            dict_shopee_product['price       '] = "price : "+str(int(item['item_basic']['price'])/100000)+"บาท"
+            dict_shopee_product['originPrice '] = int(item['item_basic']['price'])/100000
+
+        taillink = item['item_basic']['name']
+        newtaillink = taillink.replace(" ","-")
+        Product_link = URL_LINK_ITEM_SHOPEE+newtaillink
+        Product_link = Product_link +'-i.'+ str(item['item_basic']['shopid'])+'.'+str(item['item_basic']['itemid'])
+        dict_shopee_product["Product Link"] = Product_link
+        a_float = float(item['item_basic']['item_rating']['rating_star'])
+        formatted_float = '{:.2f}'.format(a_float)
+        dict_shopee_product['Rating      '] = formatted_float
+        Shopee_ll.append(dict_shopee_product)
+
+    #Sort Price Min->Max
+    if keyword_sort == 1:
+        Shopee_ll.sortPrice_min_to_max()
+
+    #Sort Price Max->Min
+    elif keyword_sort == 2:
+        Shopee_ll.sortPrice_max_to_min()
+
+    #Sort Rating Min->Max
+    elif keyword_sort == 3:
+        Shopee_ll.sortRating_min_to_max()
+    #Sort Rating Max->Min
+    elif keyword_sort == 4:
+        Shopee_ll.sortRating_max_to_min()
+
+    Shopee_ll.output()
+
+
+def JD_Central(res_Jd_central,keyword_sort):
+    JD_Central_ll = link_list()  
+
+    print('\n*****JD_CENTRAL*****\n')
+    for item in res_Jd_central["wareInfo"]:
+        dict_jd_product = {}
+        dict_jd_product["Image_link  "] = item['imageurl']
+        dict_jd_product["name        "] = item['wname']
+        try:
+            dict_jd_product['price       '] = 'original_price : '+item['originPrice']+'  บาท '+' JD price : '+item["jdPrice"]+'  บาท'
+            dict_jd_product['originPrice '] = int(item['originPrice'])
+        except KeyError:
+            dict_jd_product['price       '] = 'JD price : '+item["jdPrice"]+'  บาท'
+            dict_jd_product['originPrice '] = int(item['jdPrice'])
+        wname_i = item['wname']
+        wname_i = wname_i.replace(" ","%20")
+        dict_jd_product["Product Link"] = URL_LINK_ITEM_JD_CENTRAL.format(wname = wname_i,wareId = item['wareId'])
+        dict_jd_product['Rating      '] = float(item['starLevel'])
+        JD_Central_ll.append(dict_jd_product)
+        
+    #Sort Price Min->Max
+    if keyword_sort == 1:
+        JD_Central_ll.sortPrice_min_to_max()
+
+    #Sort Price Max->Min
+    elif keyword_sort == 2:
+        JD_Central_ll.sortPrice_max_to_min()
+
+    #Sort Rating Min->Max
+    elif keyword_sort == 3:
+        JD_Central_ll.sortRating_min_to_max()
+    #Sort Rating Max->Min
+    elif keyword_sort == 4:
+        JD_Central_ll.sortRating_max_to_min()
+
+    JD_Central_ll.output()
 
 if __name__ == "__main__":
-
-    keyword_search = input("search : ")
-    print("Rating :rx mean sort Max to Min")
-    print("Rating :rn mean sort Min to Max")
-    keyword_sort = input("PLEASE ENTER YOUR SORT : ")
+    keyword_search = input("Enter item name for searching : ")
+    print('Sort Price : \n  Case1 Min -> Max (Input 1): \n  Case2 Max -> Min (Input 2): \nSort Rating : \n  Case3 Min -> Max (Input 3): \n  Case4 Max -> Min (Input 4):')
+    keyword_sort = int(input('Enter Type Sort : '))
     print('-' * 100)
 
     keyword_shopee = urllib.parse.quote(keyword_search)
@@ -105,57 +215,9 @@ if __name__ == "__main__":
     URL_SEARCH_JD_CENTRAL = URL_SEARCH_JD_CENTRAL.format(keyword =keyword_jd_central)
     
 
-    res_Jd_central = requests.get(URL_SEARCH_JD_CENTRAL,headers=HEADER_JD_CENTRAL).json()
+    res_Jd_central = requests.get(URL_SEARCH_JD_CENTRAL,headers=HEADER_JD_CENTRAL).json()  
 
+    shopee(res_shopee,keyword_sort)
 
-    Shopee_ll = linklist()
-    JD_ll = linklist()
-
-    print('\n*****SHOPEE*****\n')
-    for item in res_shopee["items"]:
-        dict_shopee_product = {}
-        image_link = (URL_IMAGE_SHOPEE+str(item['item_basic']['image']))
-        dict_shopee_product["Image_link  "] = image_link
-        dict_shopee_product["name        "] = item['item_basic']['name']
-        if (int(item['item_basic']['price_min']) != int(item['item_basic']['price_max'])):
-            dict_shopee_product['price       '] = "price min : "+str(int(item['item_basic']['price_min'])/100000)+"บาท |  price max : "+str(int(item['item_basic']['price_max'])/100000) +"บาท"
-        else:
-            dict_shopee_product['price       '] = "price : "+str(int(item['item_basic']['price'])/100000)+"บาท"
-        taillink = item['item_basic']['name']
-        newtaillink = taillink.replace(" ","-")
-        Product_link = URL_LINK_ITEM_SHOPEE+newtaillink
-        Product_link = Product_link +'-i.'+ str(item['item_basic']['shopid'])+'.'+str(item['item_basic']['itemid'])
-        dict_shopee_product["Product Link"] = Product_link
-        a_float = float(item['item_basic']['item_rating']['rating_star'])
-        formatted_float = "{:.2f}".format(a_float)
-        dict_shopee_product["Rating      "] = formatted_float
-        Shopee_ll.append(dict_shopee_product)
-    if keyword_sort == "rx":
-        Shopee_ll.sortMax()
-    elif keyword_sort == "rn":
-        Shopee_ll.sortMin()
-    Shopee_ll.output() 
-  
-
-    print('\n*****JD_CENTRAL*****\n')
-    for item in res_Jd_central["wareInfo"]:
-        dict_jd_product = {}
-        dict_jd_product["Image_link  "] = item['imageurl']
-        dict_jd_product["name        "] = item['wname']
-        try:
-            dict_jd_product['price       '] = 'original_price : '+item['originPrice']+'  บาท '+' JD price : '+item["jdPrice"]+'  บาท'
-        except KeyError:
-            dict_jd_product['price       '] = 'JD price : '+item["jdPrice"]+'  บาท'
-        wname_i = item['wname']
-        wname_i = wname_i.replace(" ","%20")
-        dict_jd_product["Product Link"] = URL_LINK_ITEM_JD_CENTRAL.format(wname = wname_i,wareId = item['wareId'])
-        dict_jd_product["Rating      "] = float(item['starLevel'])
-        JD_ll.append(dict_jd_product)
-
-    if keyword_sort == "rx":
-        JD_ll.sortMax()
-    elif keyword_sort == "rn":
-        JD_ll.sortMin()
-    JD_ll.output()
-        
-   
+    JD_Central(res_Jd_central,keyword_sort)
+    
